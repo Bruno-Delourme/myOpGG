ca37283211c4a2bd8294444e4c22f0d5c08d808c
mimiamsa <nelly.amsalem@prosehair.com>
2024-02-19 14:39:57 +0100

    chore: last structure polishing

--- src/components/SearchBar/searchBar.jsx
+++ src/components/SearchBar/searchBar.jsx
@@ -1,14 +1,9 @@
-import { useState } from "react";
 import PropTypes from "prop-types";
 
 import "./searchBar.css";
-import backgroundVideoJayce from "../../assets/medias/videos/JAYCE_AND_GEM.mp4";
-import confirmationVideoJayce from "../../assets/medias/videos/JAYCE_ARC_SOUND.mp4";
-import backgroundVideoJinx from "../../assets/medias/videos/Shyzo_Jinx_effect.mp4";
-import confirmationVideoJinx from "../../assets/medias/videos/Jinx_appears.mp4";
 
-const SearchBar = ({ bgMode }) => {
-  const [submit, setSubmit] = useState(false);
+// keep in mind a searchbar is just a searchbar and its purpose is not to be a background home screen
+const SearchBar = ({ setSubmit }) => {
   const handleKeyDown = (event) => {
     if (event.key === "Enter") {
       setSubmit(true);
@@ -18,29 +13,9 @@ const SearchBar = ({ bgMode }) => {
   const handleSearch = () => {
     setSubmit(true);
   };
-  // conditioning the video depending on mode
-  const backgroundValue =
-    bgMode === "jayce" ? backgroundVideoJayce : backgroundVideoJinx;
-  const backgroundValueClicked =
-    bgMode === "jayce" ? confirmationVideoJayce : confirmationVideoJinx;
 
   return (
     <div className="video-container">
-      <video key={bgMode} autoPlay loop muted className="backgroundVideo">
-        <source src={backgroundValue} type="video/mp4" />
-      </video>
-      {submit && (
-        <video
-          // the key on video triggers rerender when value is changing
-          key={bgMode}
-          autoPlay
-          onEnded={() => setSubmit(false)}
-          className="confirmationVideo"
-        >
-          <source src={backgroundValueClicked} type="video/mp4" />
-        </video>
-      )}
-
       <div className="search-container">
         <select>
           <option value="euw">EUW</option>
@@ -58,11 +33,8 @@ const SearchBar = ({ bgMode }) => {
   );
 };
 
-SearchBar.defaultProps = {
-  bgMode: "jayce",
-};
 SearchBar.propTypes = {
-  bgMode: PropTypes.oneOf(["jayce", "jinx"]),
+  setSubmit: PropTypes.func.isRequired,
 };
 
 export default SearchBar;
--- src/pages/HomeScreen.jsx
+++ src/pages/HomeScreen.jsx
@@ -2,14 +2,41 @@ import { useState } from "react";
 import Footer from "../components/Footer/Footer";
 import Header from "../components/Header/Header";
 import SearchBar from "../components/SearchBar/SearchBar";
+import backgroundVideoJayce from "../assets/medias/videos/JAYCE_AND_GEM.mp4";
+import confirmationVideoJayce from "../assets/medias/videos/JAYCE_ARC_SOUND.mp4";
+import backgroundVideoJinx from "../assets/medias/videos/Shyzo_Jinx_effect.mp4";
+import confirmationVideoJinx from "../assets/medias/videos/Jinx_appears.mp4";
 
 export const HomeScreen = () => {
   // by default bgMode is set to jayce
   const [bgMode, setBgMode] = useState("jayce");
+  const [submit, setSubmit] = useState(false);
+
+  // conditioning the video depending on mode
+  const backgroundValue =
+    bgMode === "jayce" ? backgroundVideoJayce : backgroundVideoJinx;
+  const backgroundValueClicked =
+    bgMode === "jayce" ? confirmationVideoJayce : confirmationVideoJinx;
+
   return (
-    <div>
-      {/* passing the value necessary to determine which background we want */}
-      <SearchBar bgMode={bgMode} />
+    <div className="video-container">
+      {/* @todo: your mission: making a VideoBackground component */}
+      <video key={bgMode} autoPlay loop muted className="backgroundVideo">
+        <source src={backgroundValue} type="video/mp4" />
+      </video>
+      {submit && (
+        <video
+          // the key on video triggers rerender when value is changing
+          key={bgMode}
+          autoPlay
+          onEnded={() => setSubmit(false)}
+          className="confirmationVideo"
+        >
+          <source src={backgroundValueClicked} type="video/mp4" />
+        </video>
+      )}
+      {/* passing the setState from submit to searchBar we need the submit value here because we use it here */}
+      <SearchBar setSubmit={setSubmit} />
       {/* passing the set function to header as its the trigger of the changing mode is */}
       <Header setBgMode={setBgMode} />
       <Footer />
