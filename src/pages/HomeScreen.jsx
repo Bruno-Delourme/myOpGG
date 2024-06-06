8a47c112a8a7f21b4577f3d8e58f7614523eeed2
Bruno-Delourme <delourmebruno@gmail.com>
2024-06-06 18:22:17 +0200

    return to the previous version

--- src/pages/HomeScreen.jsx
+++ src/pages/HomeScreen.jsx
@@ -6,6 +6,7 @@ import backgroundVideoJayce from "../assets/medias/videos/JAYCE_AND_GEM.mp4";
 import confirmationVideoJayce from "../assets/medias/videos/JAYCE_ARC_SOUND.mp4";
 import backgroundVideoJinx from "../assets/medias/videos/Jinx_Fond.mp4";
 import confirmationVideoJinx from "../assets/medias/videos/Jinx_effect_01.mp4";
+
 import { Canvas } from '@react-three/fiber';
 import Objet3D from "../components/Objet3D/Objet3D"; //chemin correct
 
@@ -21,8 +22,9 @@ export const HomeScreen = () => {
     bgMode === "jayce" ? confirmationVideoJayce : confirmationVideoJinx;
 
   return (
+    <>
+    <div className="backgroundImage"></div>
     <div className="video-container">
-      {/* créer un composant background */}
       <video key={bgMode} autoPlay loop muted className="backgroundVideo">
         <source src={backgroundValue} type="video/mp4" />
       </video>
@@ -42,12 +44,13 @@ export const HomeScreen = () => {
         <pointLight position={[10, 10, 10]} />
         <Objet3D />
       </Canvas>
-
-      {/* on passe  de l'état submit à searchbar car on a besoin de la value submit ici parce qu'on l'utilise */}
+      
+      <div className="content">
       <SearchBar setSubmit={setSubmit} />
-      {/* on passe la set function au header pour que ça trigger le changement de mode */}
       <Header setBgMode={setBgMode} />
       <Footer />
+      </div>
     </div>
+    </>
   );
 };
